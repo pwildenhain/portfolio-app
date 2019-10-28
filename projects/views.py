ad23a387dfe9fddd6594b8d25552e127f532b26f
wildenhaip <WILDENHAIP@EMAIL.CHOP.EDU>
2019-10-28 16:45:37 -0400

    :truck: Move HTML for view into separate directory

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def project_list(request):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, 'projects/index.html')
