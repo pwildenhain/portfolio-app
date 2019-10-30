4f8f0a212bde0adbed763c81124fe37c509aec84
wildenhaip <WILDENHAIP@EMAIL.CHOP.EDU>
2019-10-30 11:01:29 -0400

    :mag: Add a view that queries the db

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from projects.models import Project
 # Create your views here.
-def project_list(request):
-    return render(request, 'projects/index.html')
+def all_projects(request):
+    # Query the db for all projects
+    projects = Project.objects.all()
+    return render(
+        request, 'projects/all_projects.html', {'projects': projects}
+    )
