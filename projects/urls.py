fd2922aa4a6322a8cddce22c416ace6868af500c
wildenhaip <WILDENHAIP@EMAIL.CHOP.EDU>
2019-10-30 13:40:14 -0400

    :bowing_man: Obey the black style

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from projects import views
 
 urlpatterns = [
-    path('', views.all_projects),
+    path("", views.all_projects),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 
 from projects.models import Project
+
 # Create your views here.
 def all_projects(request):
     # Query the db for all projects
     projects = Project.objects.all()
-    return render(
-        request, 'projects/all_projects.html', {'projects': projects}
-    )
+    return render(request, "projects/all_projects.html", {"projects": projects})
