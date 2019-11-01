70eb64996e38b03e99986bd5729e868e056c07a9
wildenhaip <WILDENHAIP@EMAIL.CHOP.EDU>
2019-11-01 10:43:51 -0400

    :eyes: Add app_name and path name to project urls for detail

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from projects import views
 
+app_name = "projects"
+
 urlpatterns = [
     path("", views.all_projects),
-    path("<int:project_id>", views.project_detail),
+    path("<int:project_id>", views.project_detail, name="project_detail"),
 ]
