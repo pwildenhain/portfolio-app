de73624d87250f527cabf2d0e0e12dfb5f6e603b
wildenhaip <WILDENHAIP@EMAIL.CHOP.EDU>
2019-11-01 10:34:52 -0400

    :mag: Add view + template for project detail page

--- projects/views.py
+++ projects/views.py
@@ -7,3 +7,7 @@ def all_projects(request):
     # Query the db for all projects
     projects = Project.objects.all()
     return render(request, "projects/all_projects.html", {"projects": projects})
+
+def project_detail(request, project_id):
+    project = Project.objects.get(pk=project_id)
+    return render(request, "projects/project_detail.html", {"project": project})
